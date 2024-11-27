91051095aafd58666faa16fc13a192dbdbc89127
sskashya <156260545+sskashya@users.noreply.github.com>
2024-11-27 01:19:31 +0000

    trying to incorporate login page with app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-workout = st.Page("workout.py", title= "workout")
+login = st.Page("login_auth".py, title = "Login", default = True)
+workout = st.Page("workout.py", title= "workout", )
 
-pg = st.navigation([workout])
+pg = st.navigation([login, workout])
 st.set_page_config(page_title="Document", page_icon=":material/edit:")
 pg.run()
