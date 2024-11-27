37c9499c5f5c1a47841c90a4fa25e572dad98df3
hjiang43 <hjiang43@syr.edu>
2024-11-27 19:44:52 +0000

    login path fix

--- login_auth.py
+++ login_auth.py
@@ -64,7 +64,7 @@ st.header("Sign up to build workouts or Login to access your account")
 
 login = st.selectbox("Login/ Sign Up", ['Select an option', 'Login', 'Sign Up'])
 
-directory_path = "/workspaces/workout-project"
+directory_path = "workout-project"
 csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
 
 if "User_Credentials.csv" in csv_files:
