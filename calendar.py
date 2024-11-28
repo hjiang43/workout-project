c4806ff7dd279111a3f79fc9b2c2861b8b37f1e0
sskashya <156260545+sskashya@users.noreply.github.com>
2024-11-28 14:55:49 +0000

    added calendar py file to app

---
+++ calendar.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import streamlit_calendar as stc
+
+st.title("View your workouts in a calendar")
+
+stc.calendar()
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 login = st.Page("login_auth.py", title = "Login", default = True)
 workout = st.Page("workout.py", title= "workout", )
 analysis = st.Page("analysis.py", title= "analysis")
+calendar = st.Page("calendar.py", title = "calendar")
 
-pg = st.navigation([login, workout, analysis])
+pg = st.navigation([login, workout, analysis, calendar])
 st.set_page_config(page_title="Document", page_icon=":material/edit:")
 pg.run()
